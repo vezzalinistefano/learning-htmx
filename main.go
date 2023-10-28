5490a3d725b2667f8893efdf28f3546cfd1ae93f
vezzalinistefano <ste.vezzalini@gmail.com>
2023-10-28 13:24:19 +0200

    feat: redirect / to /contacts

--- main.go
+++ main.go
@@ -4,24 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 
-    "github.com/vezzalinistefano/learning-htmx/repositories"
+	"github.com/vezzalinistefano/learning-htmx/repositories"
 )
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-    contactRepository := repositories.ContactsRepository
+	contactRepository := repositories.ContactsRepository
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Hello World",
-		})
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusPermanentRedirect, "/contacts")
 	})
 
-	router.GET("/contacts", func(c *gin.Context) {
-        c.JSON(http.StatusOK, contactRepository.GetAll())
+	router.GET("/contacts", func(ctx *gin.Context) {
+		ctx.HTML(
+			http.StatusOK,
+			"index",
+			gin.H{
+				"title":   "Contacts",
+				"payload": contactRepository.GetAll(),
+			},
+		)
 	})
-
 	router.Run(":8080")
 }
